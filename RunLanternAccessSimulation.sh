b6406a1ae201387c4d1d676bf11558f69e95cdee
FTGremlin <mo.shawwa@ft.com>
2016-01-12 15:45:43 +0000

    removed echo

--- RunLanternAccessSimulation.sh
+++ RunLanternAccessSimulation.sh
@@ -12,6 +12,6 @@ CT="\"Content-Type: application/json\""
 JSON="'"{\"text\":\""$TEXT"\",\"username\":\""$USERNAME"\",\"icon_emoji\":\""$ICON_EMOJI"\"}"'"
 SLACK_CHANNEL_URL="https://hooks.slack.com$HOME_CHANNEL"
 
-echo curl -H $CT -X POST -d $JSON $SLACK_CHANNEL_URL
+curl -H $CT -X POST -d $JSON $SLACK_CHANNEL_URL
 sleep 60
 mvn gatling:execute -Dgatling.simulationClass=LanternAccessSimulation -Dusers=$LANTERN_USERS -Dramp-up-minutes=$RAMP_UP_MINUTES -Dsoak-duration-minutes=$SOAK_DURATION_MINUTES
