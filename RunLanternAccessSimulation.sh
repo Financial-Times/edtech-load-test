8fc20e3d7aad3da5314aad26e9539bba70145855
FTGremlin <mo.shawwa@ft.com>
2016-01-12 16:09:00 +0000

    updated pom.xml and made sleep variable

--- RunLanternAccessSimulation.sh
+++ RunLanternAccessSimulation.sh
@@ -4,14 +4,18 @@ LANTERN_USERS=10
 RAMP_UP_MINUTES=1
 SOAK_DURATION_MINUTES=5
 
-TEXT="In 60 seconds: Running $LANTERN_USERS users through Lantern over $RAMP_UP_MINUTES min"
+SLEEP_TIME=30
+TEXT="In $SLEEP_TIME seconds: Running $LANTERN_USERS users through Lantern over $RAMP_UP_MINUTES min"
 USERNAME="gatling-bot"
 ICON_EMOJI=":gatling:"
 
-CT="\"Content-Type: application/json\""
-JSON="'"{\"text\":\""$TEXT"\",\"username\":\""$USERNAME"\",\"icon_emoji\":\""$ICON_EMOJI"\"}"'"
 SLACK_CHANNEL_URL="https://hooks.slack.com$HOME_CHANNEL"
 
-curl -H $CT -X POST -d $JSON $SLACK_CHANNEL_URL
-sleep 60
+sleep 2
+
+curl -X POST -H "Content-Type:application/json" \
+    --data '{ "text":"'"$TEXT"'", "username":"'"$USERNAME"'", "icon_emoji":"'"$ICON_EMOJI"'" }' $SLACK_CHANNEL_URL
+
+
+sleep $SLEEP_TIME
 mvn gatling:execute -Dgatling.simulationClass=LanternAccessSimulation -Dusers=$LANTERN_USERS -Dramp-up-minutes=$RAMP_UP_MINUTES -Dsoak-duration-minutes=$SOAK_DURATION_MINUTES
