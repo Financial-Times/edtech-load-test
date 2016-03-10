3407c417a64fd3dc217dd87f5e279dc331da84b4
FTGremlin <mo.shawwa@ft.com>
2016-03-10 11:00:16 +0000

    bracket error

--- src/test/scala/TestChains/Realtime48hr.scala
+++ src/test/scala/TestChains/Realtime48hr.scala
@@ -28,23 +28,23 @@ object Realtime48hr {
       .exec(http("HTTP: Confirm SID (48hr)")
         .get("/socket.io/?EIO=3&transport=polling&t=" + "LA1" + RandomGenerator.string(4) + "&sid=${sid}")
         .check(status.is(200)))
-      .exec(http("HTTP: POST Subscribe to article (48hr)")
-        .post("/socket.io/?EIO=3&transport=polling&t=" + "LA1" + RandomGenerator.string(4) + "&sid=${sid}")
-        .body(StringBody("89:42[\"subscribeToArticle\",{\"uuid\":\"${uuid}\",\"timespan\":\"48h\"}]"))
-        .check(status.is(200)))
-      .exec(http("HTTP: Confirm subscription (48hr)")
-        .get("/socket.io/?EIO=3&transport=polling&t=" + "LA1" + RandomGenerator.string(4) + "&sid=${sid}")
-        .check(status.is(200)))
+        .exec(http("HTTP: POST Subscribe to article (48hr)")
+          .post("/socket.io/?EIO=3&transport=polling&t=" + "LA1" + RandomGenerator.string(4) + "&sid=${sid}")
+          .body(StringBody("89:42[\"subscribeToArticle\",{\"uuid\":\"${uuid}\",\"timespan\":\"48h\"}]"))
+          .check(status.is(200)))
+        .exec(http("HTTP: Confirm subscription (48hr)")
+          .get("/socket.io/?EIO=3&transport=polling&t=" + "LA1" + RandomGenerator.string(4) + "&sid=${sid}")
+          .check(status.is(200)))
 
-      .exec(ws("Web Socket: Connect (48hr)").open("/?EIO=3&transport=websocket&sid=${sid}"))
-      .exec(ws("Web Socket: Send 2probe (48hr)").sendText("2probe").check(wsAwait.within(3).until(1).regex("3probe")))
-      .exec(ws("Web Socket: Send 5 (48hr)").sendText("5"))
+        .exec(ws("Web Socket: Connect (48hr)").open("/?EIO=3&transport=websocket&sid=${sid}"))
+        .exec(ws("Web Socket: Send 2probe (48hr)").sendText("2probe").check(wsAwait.within(3).until(1).regex("3probe")))
+        .exec(ws("Web Socket: Send 5 (48hr)").sendText("5"))
 
-      .forever() {
-        exec(ws("Web Socket: Send 2, Receive 3 (48hr)").sendText("2").check(wsAwait.within(3).until(1).regex("3")))
-          .exec(pause(20))
-          .exec(ws("Web Socket: Send 2, Receive 3 (48hr)").sendText("2").check(wsAwait.within(3).until(1).regex("3")))
-          .exec(ws("Web Socket: 42 Response (48hr)").check(wsAwait.within(120).until(1).regex("(42.*)")))
-      }
+        .forever() {
+          exec(ws("Web Socket: Send 2, Receive 3 (48hr)").sendText("2").check(wsAwait.within(3).until(1).regex("3")))
+            .exec(pause(20))
+            .exec(ws("Web Socket: Send 2, Receive 3 (48hr)").sendText("2").check(wsAwait.within(3).until(1).regex("3")))
+            .exec(ws("Web Socket: 42 Response (48hr)").check(wsAwait.within(120).until(1).regex("(42.*)")))
+        }
   }
 }
